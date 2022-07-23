5e0963b37145f0aafe590966be1a2a1db63dd7b8
LlaneroHiboreo <joan.gaztelu.camps.3@gmail.com>
2022-07-23 21:59:39 +0200

    1. Token Balances

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,11 +11,16 @@ contract Token{
     uint256 public decimals = 18;
     // Define supply
     uint256 public totalSupply;
+    
+    // TRACK BALANCES //
+    // Define mapping structure to link key Address and value int of type uint256
+    mapping(address=>uint256) public balanceOf;
 
     // Create constructor
     constructor(string memory _name, string memory _symbol, uint256 _totalsupp){
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _totalsupp * (10 ** decimals);
+        name = _name; // Specify token name
+        symbol = _symbol; // Specify token symbol
+        totalSupply = _totalsupp * (10 ** decimals); // Specify token total supply
+        balanceOf[msg.sender] = totalSupply; // Send all tokens created (totalSupply) to the balance address that is deploying the smart contract
     }   
 }
--- test/Token.js
+++ test/Token.js
@@ -1,18 +1,24 @@
 const { expect } = require('chai'); //extracting expect function from chai lib
 const { ethers } = require('hardhat'); //extracting ethers from hardhat lib
-const { each } = require('lodash');
+
 
 const tokens = (n) => {
     return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe('Token', ()=> {
-    let token
+    // define variables
+    let token, accounts, deployer
+    
     // NO DUPLICATE CODE: Before each test (it), the token is deployed and fetch. No need to do it for every test
     beforeEach(async ()=>{
         // Step1: Fetch token from blockchain
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy('Dapp University', 'DAPP', '1000000')
+        // Step2: Fetch deployer address
+        accounts = await ethers.getSigners()
+        deployer = accounts[0] // array to select first address from the signers(is the one used to deploy the smart contract)
+    
     })
 
     describe('Deployment', ()=>{
@@ -25,34 +31,32 @@ describe('Token', ()=> {
 
          // CHeck if name is correct
         it('Token has correct name', async ()=>{
-            // Step2: Read token name
-            const name = await token.name()
-            // Step3: Check Name is Correct
-            expect(name).to.equal(name_var)
+            // Step2: Check Name is Correct
+            expect(await token.name()).to.equal(name_var)
         })
 
         // Check if token symbol is correct
         it('Token has correct symbol', async ()=>{
-            // Step2: Read token Symbol
-            const symbol = await token.symbol()
-            // Step3: Check Symbol is Correct
-            expect(symbol).to.equal(sym_var)
+            // Step2: Check Symbol is Correct
+            expect(await token.symbol()).to.equal(sym_var)
         })
 
         // Check if decimals is correct
         it('Token has correct decimals', async ()=>{
-            // Step2: Read token Symbol
-            const decimals = await token.decimals()
-            // Step3: Check Symbol is Correct
-            expect(decimals).to.equal(dec_var)
+            // Step2: Check Symbol is Correct
+            expect(await token.decimals()).to.equal(dec_var)
         })
         
         // Check if supply is correct
+        it('Token has correct total supply', async ()=>{
+            // Step2: Check Symbol is Correct
+            expect(await token.totalSupply()).to.equal(supp_var)
+        })
+
+        // Assign total supply to deploying address
         it('Token has correct decimals', async ()=>{
-            // Step2: Read token Symbol
-            const total_supply = await token.totalSupply()
-            // Step3: Check Symbol is Correct
-            expect(total_supply).to.equal(supp_var)
+            // Step2: Check Symbol is Correct
+            expect(await token.balanceOf(deployer.address)).to.equal(supp_var)
         })
     })
 })
